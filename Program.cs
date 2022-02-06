bde919b375a9e3a804936b8c356d19f44df3c65d
burmaraider <47680616+burmaraider@users.noreply.github.com>
2022-02-06 16:41:55 -0700

    0.1

---
+++ Program.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Numerics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AVP2MemorySniffer
+{
+    class Program
+    {
+
+        public enum GameState
+        {
+            InGame = 0x88,
+            PauseMenu = 0xA0,
+            MainMenu = 0xC8,
+            Loading = 0x98,
+            GameNotLoaded = 0x0
+        };
+        
+        static void Main(string[] args)
+        {
+            bool launched = false;
+            Process _process = null;
+            ProcessMemory pm = new ProcessMemory();
+
+            Vector3 lastPosition = Vector3.Zero;
+            float lastTime = 0.0f;
+            while (true)
+            {
+                //Console.Clear();
+                if (!launched)
+                {
+                    Console.Clear();
+                    Console.SetCursorPosition(0, 0);
+                    Console.WriteLine("Waiting for AVP2 to be launched");
+                    var tempProcesses = Process.GetProcessesByName("lithtech");
+                    if (tempProcesses.Length < 1) continue;
+                    _process = tempProcesses[0];
+                    pm = new ProcessMemory(_process, (IntPtr)_process.Id);
+                    launched = true;
+                    continue;
+                }
+
+                if((int)pm.getBaseAddress == 0)
+                {
+                    launched = false;
+                    continue;
+                }
+
+                Console.SetCursorPosition(0, 0);
+                ClearCurrentConsoleLine();
+                Console.WriteLine("AVP2 Running");
+
+
+                bool objectlto = false;
+                bool d3dren = false;
+                _process.Refresh();
+                try
+                {
+                    var levelName = pm.ReadStringASCII((IntPtr)pm.GetModuleAddress("object.lto") + 0x2FD9B4, 32);
+
+                    if (levelName != string.Empty)
+                    {
+                        Console.SetCursorPosition(0, 1);
+                        ClearCurrentConsoleLine();
+                        Console.WriteLine(String.Format("Level loaded: {0}", levelName));
+                    }
+
+                    byte loading = pm.ReadByte((IntPtr)pm.GetModuleAddress("d3d.ren") + 0x55E73);
+
+                    var status = loading > 0 ? "false" : "true";
+                    Console.SetCursorPosition(0, 2);
+                    ClearCurrentConsoleLine();
+                    Console.WriteLine(String.Format("Loading: {0}", status));
+
+
+                    loading = pm.ReadByte((IntPtr)pm.GetModuleAddress("d3d.ren") + 0x5627C);
+                    Console.SetCursorPosition(0, 3);
+                    ClearCurrentConsoleLine();
+                    Console.WriteLine(String.Format("Status: {0}", Enum.GetName(typeof(GameState), loading)));
+
+                    var headCount = pm.TraverseInt32((IntPtr)pm.GetModuleAddress("cshell.dll") + 0x1c5868, new int[] { 0x798 });
+                    Console.SetCursorPosition(0, 4);
+                    ClearCurrentConsoleLine();
+                    Console.WriteLine(String.Format("Head Trophys: {0}", headCount));
+
+                    var health = pm.TraverseInt32((IntPtr)pm.GetModuleAddress("cshell.dll") + 0x1c5868, new int[] { 0x798 }, -0x10);
+                    Console.SetCursorPosition(0, 5);
+                    ClearCurrentConsoleLine();
+                    Console.WriteLine(String.Format("Health: {0}", health));
+
+
+                    float x = (float)pm.TraverseFloat((IntPtr)pm.getBaseAddress + 0xE4994, new int[] { 0x14, 0x414, 0x7d4 });
+                    float y = (float)pm.TraverseFloat((IntPtr)pm.getBaseAddress + 0xE4994, new int[] { 0x14, 0x414, 0x7d4 }, 4);
+                    float z = (float)pm.TraverseFloat((IntPtr)pm.getBaseAddress + 0xE4994, new int[] { 0x14, 0x414, 0x7d4 }, 8);
+                    // lithtech.exe + E49A0
+
+                    Vector3 newPos = new Vector3(x, y, z);
+                    Console.SetCursorPosition(0, 6);
+                    ClearCurrentConsoleLine();
+                    Console.WriteLine(String.Format("Position {0:0.00}, {1:0.00}, {2:0.00}", newPos.X, newPos.Y, newPos.Z));
+
+
+                    var currentTime = (float)pm.ReadFloat((IntPtr)pm.getBaseAddress + 0xE49A0);
+
+                    float deltaTime = currentTime - lastTime;
+                    Vector3 deltaPos = newPos - lastPosition;
+
+                    Console.SetCursorPosition(0, 8);
+                    ClearCurrentConsoleLine();
+                    Console.WriteLine(String.Format("delta {0:0.00}", deltaTime));
+
+                    if (deltaTime < 1.4f)
+                    {
+                        Vector3 velocity = deltaPos / deltaTime;
+                        float sum = velocity.X * velocity.X + velocity.Y * velocity.Y + velocity.Z * velocity.Z;
+                        Console.SetCursorPosition(0, 7);
+                        ClearCurrentConsoleLine();
+                        
+                        Console.WriteLine(String.Format("Speed {0:0.00}", Math.Sqrt(sum) * 40));
+                        
+                    }
+
+                    lastTime = currentTime;
+                    lastPosition = newPos;
+
+                }
+                catch
+                { }
+
+            }
+        }
+        public static void ClearCurrentConsoleLine()
+        {
+            int currentLineCursor = Console.CursorTop;
+            Console.SetCursorPosition(0, Console.CursorTop);
+            Console.Write(new string(' ', Console.WindowWidth));
+            Console.SetCursorPosition(0, currentLineCursor);
+        }
+       
+    }
+}
---
+++ ProgramMemory.cs
@@ -0,0 +1,345 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Text;
+
+public class ProcessMemory
+{
+    [DllImport("kernel32.dll", SetLastError = true)]
+    private static extern bool ReadProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, byte[] lpBuffer, uint dwSize, uint lpNumberOfBytesRead);
+
+    [DllImport("kernel32.dll")]
+    private static extern bool WriteProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, byte[] lpBuffer, uint nSize, uint lpNumberOfBytesWritten);
+
+    [DllImport("kernel32.dll")]
+    private static extern IntPtr OpenProcess(uint dwDesiredAccess, bool bInheritHandle, int dwProcessId);
+
+    [DllImport("kernel32.dll")]
+    private static extern bool GetExitCodeProcess(IntPtr hObject, out uint lpExitCode);
+
+    [DllImport("kernel32.dll")]
+    private static extern bool CloseHandle(IntPtr hObject); // unused?
+
+    [DllImport("kernel32.dll")]
+    private static extern bool VirtualProtectEx(IntPtr hProcess, IntPtr lpAddress, UIntPtr dwSize, uint flNewProtect, out uint lpflOldProtect);
+
+    [DllImport("kernel32.dll", ExactSpelling = true, SetLastError = true)]
+    private static extern IntPtr VirtualAllocEx(IntPtr hProcess, IntPtr lpAddress, uint dwSize, uint flAllocationType, uint flProtect); // unused?
+
+    [DllImport("kernel32.dll")]
+    private static extern IntPtr OpenThread(uint dwDesiredAccess, bool bInheritHandle, uint dwThreadId);
+
+    [DllImport("kernel32.dll")]
+    private static extern uint SuspendThread(IntPtr hThread);
+
+    [DllImport("kernel32.dll")]
+    private static extern int ResumeThread(IntPtr hThread);
+
+    public IntPtr baseAddress;
+    public long getBaseAddress
+    {
+        get
+        {
+            if (CheckProcess())
+            {
+                baseAddress = (IntPtr)0;
+                processModule = mainProcess[0].MainModule;
+                baseAddress = processModule.BaseAddress;
+                return (long)baseAddress;
+            }
+            else return 0;
+        }
+    }
+
+    public string processName;
+    public bool TrustProcess;
+    private bool _opened;
+    private ProcessModule processModule;
+    private Process[] mainProcess;
+    private IntPtr processHandle = IntPtr.Zero;
+
+    public ProcessMemory()
+    {
+
+    }
+    public ProcessMemory(string param, bool trust = false)
+    {
+        processName = param;
+        TrustProcess = trust;
+    }
+
+    public ProcessMemory(Process proc, IntPtr handle, bool trust = false)
+    {
+        mainProcess = new Process[] { proc };
+        processHandle = OpenProcess(0x001F0FFF, false, (int)handle);
+        _opened = true;
+        processName = "lithtech";
+        processModule = mainProcess[0].MainModule;
+    }
+
+    public int GetModuleAddress(string moduleName)
+    {
+        foreach (ProcessModule module in mainProcess[0].Modules)
+        {
+            if (module.ModuleName == moduleName)
+            {
+                return (int)module.BaseAddress;
+            }
+        }
+        return 0x0;
+    }
+
+    public DateTime GetProcessTime()
+    {
+        return mainProcess[0].StartTime;
+    }
+    public int GetProcessID()
+    {
+        return mainProcess[0].Id;
+    }
+
+    public bool CheckProcess()
+    {
+        if (TrustProcess && _opened) return true;
+        if (processName == null) return false;
+
+        if (GetExitCodeProcess(processHandle, out uint code) && code != 259)
+        {
+            CloseHandle(processHandle);
+            processHandle = IntPtr.Zero;
+        }
+
+        if (processHandle == IntPtr.Zero)
+        {
+            mainProcess = Process.GetProcessesByName(processName);
+            if (mainProcess.Length == 0) return false;
+
+            processHandle = OpenProcess(0x001F0FFF, false, mainProcess[0].Id);
+            if (processHandle == IntPtr.Zero) return false;
+        }
+
+        if (TrustProcess) _opened = true;
+        return true;
+    }
+
+    public void Suspend()
+    {
+        foreach (ProcessThread pT in mainProcess[0].Threads)
+        {
+            IntPtr pOpenThread = OpenThread(0x02 /* suspend/resume */, false, (uint)pT.Id);
+
+            if (pOpenThread == IntPtr.Zero)
+            {
+                continue;
+            }
+
+            SuspendThread(pOpenThread);
+
+            CloseHandle(pOpenThread);
+        }
+    }
+
+    public void Resume()
+    {
+        foreach (ProcessThread pT in mainProcess[0].Threads)
+        {
+            IntPtr pOpenThread = OpenThread(0x02 /* suspend/resume */, false, (uint)pT.Id);
+
+            if (pOpenThread == IntPtr.Zero)
+            {
+                continue;
+            }
+
+            var suspendCount = 0;
+            do
+            {
+                suspendCount = ResumeThread(pOpenThread);
+            } while (suspendCount > 0);
+
+            CloseHandle(pOpenThread);
+        }
+    }
+
+    public void Stop()
+    {
+        mainProcess[0].Kill();
+    }
+
+    public byte[] ReadByteArray(IntPtr addr, uint size)
+    {
+        if (!CheckProcess()) return new byte[0];
+
+        VirtualProtectEx(processHandle, addr, (UIntPtr)size, 0x40 /* rw */, out uint flNewProtect);
+
+        byte[] array = new byte[size];
+        ReadProcessMemory(processHandle, addr, array, size, 0u);
+
+        VirtualProtectEx(processHandle, addr, (UIntPtr)size, flNewProtect, out _);
+        //CloseHandle(processHandle);
+        return array;
+    }
+
+    public string ReadStringUnicode(IntPtr addr, uint size) => CheckProcess()
+        ? Encoding.Unicode.GetString(ReadByteArray(addr, size), 0, (int)size)
+        : "";
+
+    public string ReadStringASCII(IntPtr addr, uint size) => CheckProcess()
+        ? Encoding.ASCII.GetString(ReadByteArray(addr, size), 0, (int)size)
+        : "";
+
+    public char ReadChar(IntPtr addr) => CheckProcess()
+        ? BitConverter.ToChar(ReadByteArray(addr, 1), 0)
+        : ' ';
+
+    public bool ReadBoolean(IntPtr addr) => CheckProcess()
+        ? BitConverter.ToBoolean(ReadByteArray(addr, 1), 0)
+        : false;
+
+    public byte ReadByte(IntPtr addr) => CheckProcess()
+        ? ReadByteArray(addr, 1)[0]
+        : (byte)0;
+
+    public short ReadInt16(IntPtr addr) => CheckProcess()
+        ? BitConverter.ToInt16(ReadByteArray(addr, 2), 0)
+        : (short)0;
+
+    public int ReadInt32(IntPtr addr) => CheckProcess()
+        ? BitConverter.ToInt32(ReadByteArray(addr, 4), 0)
+        : 0;
+
+    public long ReadInt64(IntPtr addr) => CheckProcess()
+        ? BitConverter.ToInt64(ReadByteArray(addr, 8), 0)
+        : 0;
+
+    public ushort ReadUInt16(IntPtr addr) => CheckProcess()
+        ? BitConverter.ToUInt16(ReadByteArray(addr, 2), 0)
+        : (ushort)0;
+
+    public uint ReadUInt32(IntPtr addr) => CheckProcess()
+        ? BitConverter.ToUInt32(ReadByteArray(addr, 4), 0)
+        : 0;
+
+    public ulong ReadUInt64(IntPtr addr) => CheckProcess()
+        ? BitConverter.ToUInt64(ReadByteArray(addr, 8), 0)
+        : 0;
+
+    public float ReadFloat(IntPtr addr) => CheckProcess()
+        ? BitConverter.ToSingle(ReadByteArray(addr, 4), 0)
+        : 0f;
+
+    public double ReadDouble(IntPtr addr) => CheckProcess()
+        ? BitConverter.ToDouble(ReadByteArray(addr, 8), 0)
+        : 0.0;
+
+    public bool WriteByteArray(IntPtr addr, byte[] pBytes)
+    {
+        if (!CheckProcess()) return false;
+
+        VirtualProtectEx(processHandle, addr, (UIntPtr)pBytes.Length, 0x40 /* rw */, out uint flNewProtect);
+
+        bool flag = WriteProcessMemory(processHandle, addr, pBytes, (uint)pBytes.Length, 0u);
+
+        VirtualProtectEx(processHandle, addr, (UIntPtr)pBytes.Length, flNewProtect, out _);
+
+        return flag;
+    }
+
+    public bool WriteStringUnicode(IntPtr addr, string pData) => WriteByteArray(addr, Encoding.Unicode.GetBytes(pData));
+
+    public bool WriteStringASCII(IntPtr addr, string pData) => WriteByteArray(addr, Encoding.ASCII.GetBytes(pData));
+
+    public bool WriteBoolean(IntPtr addr, bool pData) => WriteByteArray(addr, BitConverter.GetBytes(pData));
+
+    public bool WriteChar(IntPtr addr, char pData) => WriteByteArray(addr, BitConverter.GetBytes(pData));
+
+    public bool WriteByte(IntPtr addr, byte pData) => WriteByteArray(addr, new byte[] { pData });
+
+    public bool WriteInt16(IntPtr addr, short pData) => WriteByteArray(addr, BitConverter.GetBytes(pData));
+
+    public bool WriteInt32(IntPtr addr, int pData) => WriteByteArray(addr, BitConverter.GetBytes(pData));
+
+    public bool WriteInt64(IntPtr addr, long pData) => WriteByteArray(addr, BitConverter.GetBytes(pData));
+
+    public bool WriteUInt16(IntPtr addr, ushort pData) => WriteByteArray(addr, BitConverter.GetBytes(pData));
+
+    public bool WriteUInt32(IntPtr addr, uint pData) => WriteByteArray(addr, BitConverter.GetBytes(pData));
+
+    public bool WriteUInt64(IntPtr addr, ulong pData) => WriteByteArray(addr, BitConverter.GetBytes(pData));
+
+    public bool WriteFloat(IntPtr addr, float pData) => WriteByteArray(addr, BitConverter.GetBytes(pData));
+
+    public bool WriteDouble(IntPtr addr, double pData) => WriteByteArray(addr, BitConverter.GetBytes(pData));
+
+    public bool Traverse(IntPtr addr, IEnumerable<int> offsets, out IntPtr result)
+    {
+        result = addr;
+
+        if (!CheckProcess() || result == IntPtr.Zero) return false;
+
+        foreach (int offset in offsets)
+        {
+            result = (IntPtr)ReadInt32(result);
+            if (result == IntPtr.Zero) return false;
+
+            result += offset;
+        }
+
+        return true;
+    }
+
+    public string TraverseStringUnicode(IntPtr addr, IEnumerable<int> offsets, uint size) => Traverse(addr, offsets, out IntPtr result)
+        ? ReadStringUnicode(result, size)
+        : null;
+
+    public string TraverseStringASCII(IntPtr addr, IEnumerable<int> offsets, uint size) => Traverse(addr, offsets, out IntPtr result)
+        ? ReadStringASCII(result, size)
+        : null;
+    public string TraverseStringASCII(IntPtr addr, IEnumerable<int> offsets, int final, uint size) => Traverse(addr, offsets, out IntPtr result)
+        ? ReadStringASCII(result + final, size)
+        : null;
+
+    public char? TraverseChar(IntPtr addr, IEnumerable<int> offsets) => Traverse(addr, offsets, out IntPtr result)
+        ? ReadChar(result)
+        : (char?)null;
+
+    public bool? TraverseBoolean(IntPtr addr, IEnumerable<int> offsets) => Traverse(addr, offsets, out IntPtr result)
+        ? ReadBoolean(result)
+        : (bool?)null;
+
+    public byte? TraverseByte(IntPtr addr, IEnumerable<int> offsets) => Traverse(addr, offsets, out IntPtr result)
+        ? ReadByte(result)
+        : (byte?)null;
+
+    public short? TraverseInt16(IntPtr addr, IEnumerable<int> offsets) => Traverse(addr, offsets, out IntPtr result)
+        ? ReadInt16(result)
+        : (short?)null;
+
+    public int? TraverseInt32(IntPtr addr, IEnumerable<int> offsets, int final = 0) => Traverse(addr, offsets, out IntPtr result)
+        ? ReadInt32(result + final)
+        : (int?)null;
+
+    public long? TraverseInt64(IntPtr addr, IEnumerable<int> offsets) => Traverse(addr, offsets, out IntPtr result)
+        ? ReadInt64(result)
+        : (long?)null;
+
+    public ushort? TraverseUInt16(IntPtr addr, IEnumerable<int> offsets) => Traverse(addr, offsets, out IntPtr result)
+        ? ReadUInt16(result)
+        : (ushort?)null;
+
+    public uint? TraverseUInt32(IntPtr addr, IEnumerable<int> offsets, int final = 0) => Traverse(addr, offsets, out IntPtr result)
+        ? ReadUInt32(result + final)
+        : (uint?)null;
+
+    public ulong? TraverseUInt64(IntPtr addr, IEnumerable<int> offsets) => Traverse(addr, offsets, out IntPtr result)
+        ? ReadUInt64(result)
+        : (ulong?)null;
+
+    public float? TraverseFloat(IntPtr addr, IEnumerable<int> offsets, int final = 0) => Traverse(addr, offsets, out IntPtr result)
+        ? ReadFloat(result + final)
+        : (float?)null;
+
+    public double? TraverseDouble(IntPtr addr, IEnumerable<int> offsets) => Traverse(addr, offsets, out IntPtr result)
+        ? ReadDouble(result)
+        : (double?)null;
+}
