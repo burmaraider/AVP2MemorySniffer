b5c758b02bc608c6394e0cf8924b01346ed57818
burmaraider <47680616+burmaraider@users.noreply.github.com>
2022-02-06 22:26:52 -0700

    0.1

--- Program.cs
+++ Program.cs
@@ -20,7 +20,7 @@ namespace AVP2MemorySniffer
             Loading = 0x98,
             GameNotLoaded = 0x0
         };
-        
+
         static void Main(string[] args)
         {
             bool launched = false;
@@ -40,7 +40,6 @@ namespace AVP2MemorySniffer
 
             while (true)
             {
-                //Console.Clear();
                 if (!launched)
                 {
                     Console.Clear();
@@ -54,7 +53,7 @@ namespace AVP2MemorySniffer
                     continue;
                 }
 
-                if((int)pm.getBaseAddress == 0)
+                if ((int)pm.getBaseAddress == 0)
                 {
                     launched = false;
                     continue;
@@ -74,61 +73,81 @@ namespace AVP2MemorySniffer
                     GetHealth(pm);
                     PositionSpeed(pm, ref lastPosition, ref lastTime);
 
-                bool objectlto = false;
-                bool d3dren = false;
-                _process.Refresh();
-                try
-                {
-                    var levelName = pm.ReadStringASCII((IntPtr)pm.GetModuleAddress("object.lto") + 0x2FD9B4, 32);
-
-                    if (levelName != string.Empty)
-                    {
-                        Console.SetCursorPosition(0, 1);
-                        ClearCurrentConsoleLine();
-                        Console.WriteLine(String.Format("Level loaded: {0}", levelName));
-                    }
-        }
-
-                    ClearCurrentConsoleLine();
-
-                    var headCount = pm.TraverseInt32((IntPtr)pm.GetModuleAddress("cshell.dll") + 0x1c5868, new int[] { 0x798 });
-                    Console.SetCursorPosition(0, 4);
-                    ClearCurrentConsoleLine();
-                    Console.WriteLine(String.Format("Head Trophys: {0}", headCount));
-        }
-
-                    ClearCurrentConsoleLine();
-
-        private static void PositionSpeed(ProcessMemory pm, ref Vector3 lastPosition, ref float lastTime)
-        {
-                    float x = (float)pm.TraverseFloat((IntPtr)pm.getBaseAddress + 0xE4994, new int[] { 0x14, 0x414, 0x7d4 });
-                    float y = (float)pm.TraverseFloat((IntPtr)pm.getBaseAddress + 0xE4994, new int[] { 0x14, 0x414, 0x7d4 }, 4);
-                    float z = (float)pm.TraverseFloat((IntPtr)pm.getBaseAddress + 0xE4994, new int[] { 0x14, 0x414, 0x7d4 }, 8);
-                    // lithtech.exe + E49A0
-
-                    Vector3 newPos = new Vector3(x, y, z);
-
-
-
-                    float deltaTime = currentTime - lastTime;
-                    Vector3 deltaPos = newPos - lastPosition;
-
-
-                    {
-                        Vector3 velocity = deltaPos / deltaTime;
-                        
-                        
-                    }
-
-                    lastTime = currentTime;
-                    lastPosition = newPos;
-
                 }
                 catch
                 { }
 
             }
         }
+
+        private static void GetLevelName(ProcessMemory pm)
+        {
+            var levelName = pm.ReadStringASCII((IntPtr)pm.GetModuleAddress("object.lto") + 0x2FD9B4, 32);
+
+            if (levelName != string.Empty)
+            {
+                Console.SetCursorPosition(0, 1);
+                ClearCurrentConsoleLine();
+                Console.WriteLine(String.Format("Level loaded: {0}", levelName));
+            }
+        }
+
+        private static void GetHealth(ProcessMemory pm)
+        {
+            var health = pm.TraverseInt32((IntPtr)pm.GetModuleAddress("cshell.dll") + 0x1c5868, new int[] { 0x798 }, -0x10);
+            Console.SetCursorPosition(0, 5);
+            ClearCurrentConsoleLine();
+            Console.WriteLine(String.Format("Health: {0}", health));
+        }
+
+        private static void GetHeadTrophyCount(ProcessMemory pm)
+        {
+            var headCount = pm.TraverseInt32((IntPtr)pm.GetModuleAddress("cshell.dll") + 0x1c5868, new int[] { 0x798 });
+            Console.SetCursorPosition(0, 4);
+            ClearCurrentConsoleLine();
+            Console.WriteLine(String.Format("Head Trophys: {0}", headCount));
+        }
+
+        private static byte IsLoading(ProcessMemory pm)
+        {
+            byte loading = pm.ReadByte((IntPtr)pm.GetModuleAddress("d3d.ren") + 0x5627C);
+            Console.SetCursorPosition(0, 3);
+            ClearCurrentConsoleLine();
+            Console.WriteLine(String.Format("Status: {0}", Enum.GetName(typeof(GameState), loading)));
+            return loading;
+        }
+
+        private static void PositionSpeed(ProcessMemory pm, ref Vector3 lastPosition, ref float lastTime)
+        {
+            float x = (float)pm.TraverseFloat((IntPtr)pm.getBaseAddress + 0xE4994, new int[] { 0x14, 0x414, 0x7d4 });
+            float y = (float)pm.TraverseFloat((IntPtr)pm.getBaseAddress + 0xE4994, new int[] { 0x14, 0x414, 0x7d4 }, 4);
+            float z = (float)pm.TraverseFloat((IntPtr)pm.getBaseAddress + 0xE4994, new int[] { 0x14, 0x414, 0x7d4 }, 8);
+
+            Vector3 newPos = new Vector3(x, y, z);
+            ConsoleWriteColor(String.Format("Position [{0:0.00}], [{1:0.00}], [{2:0.00}]", newPos.X, newPos.Y, newPos.Z), 0, 6, ConsoleColor.Yellow);
+
+
+            //var currentTime = (float)pm.ReadFloat((IntPtr)pm.getBaseAddress + 0xE49A0);
+            var currentTime = (float)pm.ReadFloat((IntPtr)pm.GetModuleAddress("d3d.ren") + 0x53278);
+
+            float deltaTime = currentTime - lastTime;
+            Vector3 deltaPos = newPos - lastPosition;
+
+            ConsoleWriteColor(String.Format("delta [{0:0.00}]", deltaTime), 0, 7, ConsoleColor.Yellow);
+
+            if (deltaTime <= 0.5f)
+            {
+                Vector3 velocity = deltaPos / deltaTime;
+                float sum = velocity.X * velocity.X + 0 * 0 + velocity.Z * velocity.Z;//only care about horizontal speed
+
+                ConsoleWriteColor(String.Format("Speed [{0:0.00}]", Math.Sqrt(sum) * 15), 0, 8, ConsoleColor.Yellow);
+
+            }
+
+            lastTime = currentTime;
+            lastPosition = newPos;
+        }
+
         public static void ClearCurrentConsoleLine()
         {
             int currentLineCursor = Console.CursorTop;
@@ -136,7 +155,7 @@ namespace AVP2MemorySniffer
             Console.Write(new string(' ', Console.WindowWidth));
             Console.SetCursorPosition(0, currentLineCursor);
         }
-       
+
         public static void ConsoleWriteColor(string inputString, int left, int right, ConsoleColor fgColor = ConsoleColor.White, ConsoleColor bgColor = ConsoleColor.Black)
         {
             var pieces = Regex.Split(inputString, @"(\[[^\]]*\])");
@@ -157,8 +176,8 @@ namespace AVP2MemorySniffer
                 Console.Write(piece);
                 Console.ResetColor();
             }
-       
-            
+
+
+        }
     }
-}
 }
